a5c234edbda8fc46389d6ec71a6b2de32d647f66
BurnellLiu <burnell_liu@outlook.com>
2017-09-21 15:09:25 +0800

    增加电源管理模块

---
+++ Src/PowerMgmt/PowerMgmt.cpp
@@ -0,0 +1,18 @@
+﻿
+#include "PowerMgmt.h"
+
+#include <Windows.h>
+#include <powrprof.h>
+
+#pragma comment(lib, "powrprof.lib")
+
+void PowerMgmt::EnterHibernate()
+{
+    SetSuspendState(TRUE, TRUE, FALSE);
+}
+
+
+void PowerMgmt::EnterSleep()
+{
+    SetSuspendState(FALSE, TRUE, FALSE);
+}
---
+++ Src/PowerMgmt/PowerMgmt.h
@@ -0,0 +1,12 @@
+﻿
+#pragma once
+
+
+namespace PowerMgmt
+{
+    /// @brief 进入休眠状态
+    void EnterHibernate();
+
+    /// @brief 进行睡眠状态
+    void EnterSleep();
+}
